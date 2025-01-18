23d53323375fc01f8e3549ea96c8d18f9a3c0434
Nelson R. Montenegro <montenegron002@gmail.com>
2025-01-17 19:14:43 -0500

    Setup React-Router-Dom and build layout and pages holding each component

---
+++ src/pages/homePage.page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero/hero.component";
+import HomeCards from "../components/Home-Cards/homeCards.component";
+import JobListings from "../components/JobListings/jobListings.component";
+import ViewAllJobs from "../components/ViewAllJobs/viewAllJobs.component";
+
+const HomePages = () => {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default HomePages;
